eba892259991011af95b71fa2722a70bca626aa9
2259288066@qq.com <2259288066@qq.com>
2018-08-27 14:44:56 +0800

    提交images-folder

--- Labels/ViewController.m
+++ Labels/ViewController.m
@@ -17,9 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    
-      NSArray *expert_tagArray = @[@"商务智能",@"区块链",@"IT项目管理",@"电子商",@"管理系统",@"软技能",@"信息安全",@"变革转型"];
+      NSArray *tagArray = @[@"商务智能",@"区块链",@"IT项目管理",@"电子商",@"管理系统",@"软技能",@"信息安全",@"变革转型",@"软技能",@"信息安全",@"变革转型",@"软技能",@"信息安全",@"变革转型",@"软技能",@"信息安全",@"变革转型",@"软技能",@"信息安全",@"变革转型",@"软技能",@"信息安全",@"变革转型jajaja",@"哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈"];
     
-    TagsLabels *label = [[TagsLabels alloc] initTitleArray:expert_tagArray fontSize:15 textColor:[UIColor redColor] bgColor:[UIColor grayColor] labelMargin:8 labelH:25 maxWidth:self.view.frame.size.width - 30 tagViewX:15 tagViewY:100];
+    TagsLabels *label = [[TagsLabels alloc] initTitleArray:tagArray fontSize:15 textColor:[UIColor redColor] bgColor:[UIColor clearColor] labelMargin:8 labelH:25 maxWidth:self.view.frame.size.width - 30 tagViewX:15 tagViewY:100];
     label.backgroundColor = [UIColor yellowColor];
     [self.view addSubview:label];
 }
